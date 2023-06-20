b344fce86438030831faa5bbe0afdc7566c25f94
Vitalii-Mart <110160794+Vitalii-Mart@users.noreply.github.com>
2023-06-20 20:45:04 +0300

    start

---
+++ contacts.js
@@ -0,0 +1,28 @@
+const fs = require("fs").promises;
+const path = require("path");
+const { nanoid } = require("nanoid");
+
+const contactsPath = path.join(__dirname, "db/contacts.json");
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+}
+
+async function getContactById(contactId) {
+  const data = await listContacts();
+  const result = data.find((e) => {
+    return e.contactId === contactId;
+  });
+  return result || null;
+}
+
+function removeContact(contactId) {
+  // ...твой код. Возвращает объект удаленного контакта. Возвращает null, если объект с таким id не найден.
+}
+
+function addContact(name, email, phone) {
+
+}
+
+module.exports = { listContacts, getContactById, removeContact, addContact };
